0d6b226341c52dff6579cf18245c7d9e3c3d1141
Owais Khan <oaak78692@gmail.com>
2023-09-02 11:21:29 +0530

    modified index.js

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 const port = 8080;
 const path = require("path");
 
+app.use(express.urlencoded({extended : true}));
+
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
 app.set(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+    res.send("Server Working Well!");
+})
 
 app.listen(port,()=>{
     console.log(`Listening to port ${port}`);
