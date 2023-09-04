67fc68848a25acebd9fd9ee0dc0e349f0219ecb3
Owais Khan <oaak78692@gmail.com>
2023-09-04 19:50:05 +0530

    added some styling and 'add new post' feature

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ const app = express();
 const port = 8080;
 const path = require("path");
 
+const posts=[]
+
 app.use(express.urlencoded({extended : true}));
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
 
-app.get("/",(req,res)=>{
-    res.send("Server Working Well!");
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+})
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+})
+app.post("/posts",(req,res)=>{
+    let {username,content}=req.body;
+    posts.push({username,content});
+    res.redirect("/posts");
 })
 
 app.listen(port,()=>{
